5328281d2481351d042e5a719dec23b0eb225270
pkkudo <46470104+pkkudo@users.noreply.github.com>
2021-12-11 12:05:26 +0900

    writing the result in both json and txt

--- sh_int/show_int1.py
+++ sh_int/show_int1.py
@@ -41,13 +41,19 @@ def main():
     # get information from a remote device
     with ConnectHandler(**target) as net_connect:
         net_connect.session_preparation()
+        # use ntc_template
         dct_int = net_connect.send_command_timing('show interfaces', use_textfsm=True)
+        # get raw output
+        o_int = net_connect.send_command_timing('show interfaces')
 
     # write the retrieved information to a file
     f_out = '/'.join([dir_out, target_device])
     f_out += '_show_int.json'
     with open(f_out, 'w') as salida:
         salida.write(json.dumps(dct_int, indent=2))
+    f_out = f_out[:-5] + '.txt'
+    with open(f_out, 'w') as salida:
+        salida.write(o_int)
 
     return 0
 
